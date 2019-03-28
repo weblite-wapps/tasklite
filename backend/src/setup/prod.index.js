3b95ed726373cce34470440a49e0f441dcb38596
Masoud Salehi <msk.masoud.74@gmail.com>
2019-03-28 13:04:30 +0430

    chore(backend): Remove https; change production port; Fix package name

--- backend/src/setup/prod.index.js
+++ backend/src/setup/prod.index.js
@@ -1,16 +1,6 @@
-// modules
-import path from 'path'
-import https from 'https'
-import fs from 'fs'
 // components
 import app from './server'
 import './mongodb'
 import '../logic/main/route'
 
-
-const privateKey = fs.readFileSync(path.resolve('./src/certs/express.key'), 'utf8')
-const certificate = fs.readFileSync(path.resolve('./src/certs/express.crt'), 'utf8')
-
-https
-  .createServer({ key: privateKey, cert: certificate }, app)
-  .listen(3030)
+app.listen(3130, () => console.log('Server Running!'))
