03f9bba0d80ba5ecc6c3f7b75342ad419079d296
Mostafa Mohseni Kabir <mostafa_mohsenikabir@yahoo.com>
2019-05-23 23:12:21 +0430

    fix(refactor): Add tootltip for icon buttons

--- frontend/src/components/components/AppBar/AppBar.helper.component.jsx
+++ frontend/src/components/components/AppBar/AppBar.helper.component.jsx
@@ -1,15 +1,15 @@
 // modules
-import React from 'react'
-import PropTypes from 'prop-types'
-import { CircularProgress } from 'material-ui/Progress'
-import IconButton from 'material-ui/IconButton'
-import Tooltip from 'material-ui/Tooltip'
+import React from "react";
+import PropTypes from "prop-types";
+import * as R from "ramda";
+import { CircularProgress } from "material-ui/Progress";
+import IconButton from "material-ui/IconButton";
+import Tooltip from "material-ui/Tooltip";
 // icons
-import SortIcon from 'material-ui-icons/Sort'
-import ExitIcon from 'material-ui-icons/ExitToApp'
+import SortIcon from "material-ui-icons/Sort";
+import ExitIcon from "material-ui-icons/ExitToApp";
 // styles
-import scssClasses from './AppBar.scss'
-
+import scssClasses from "./AppBar.scss";
 
 export const Logo = ({ isLoading, setAboutMode }) => (
   <React.Fragment>
@@ -20,84 +20,120 @@ export const Logo = ({ isLoading, setAboutMode }) => (
       tabIndex="0"
     >
       <div className={isLoading ? scssClasses.loading : scssClasses.normal}>
-        <CircularProgress size={40} color="primary" className={scssClasses.progress} />
-        <img alt="loglite logo" src="assets/toplogo.png" className={scssClasses.logo} />
+        <CircularProgress
+          size={40}
+          color="primary"
+          className={scssClasses.progress}
+        />
+        <img
+          alt="loglite logo"
+          src="assets/toplogo.png"
+          className={scssClasses.logo}
+        />
       </div>
     </div>
   </React.Fragment>
-)
+);
 
 Logo.propTypes = {
   isLoading: PropTypes.bool.isRequired,
-  setAboutMode: PropTypes.func.isRequired,
-}
-
+  setAboutMode: PropTypes.func.isRequired
+};
 
 export const TabBar = (props) => {
   const {
-    classes, aboutMode, setAboutMode, sortByDeadline, toggleSortByDeadline,
-  } = props
+    classes,
+    aboutMode,
+    setAboutMode,
+    sortByDeadline,
+    toggleSortByDeadline
+  } = props;
 
   return (
     <div className={scssClasses.tabBar}>
-      <img alt="loglite logo" src="assets/typo.png" className={scssClasses.typo} />
+      <img
+        alt="loglite logo"
+        src="assets/typo.png"
+        className={scssClasses.typo}
+      />
       <div className={scssClasses.iconsContainer}>
-        {
-          !aboutMode &&
-            <React.Fragment>
-              <Tooltip
-                enterDelay={300}
-                leaveDelay={300}
-                placement="bottom"
-                title={sortByDeadline ? 'Sort By Priority' : 'Sort By Deadline'}
-              >
-                <IconButton onClick={toggleSortByDeadline}>
-                  <SortIcon className={sortByDeadline ? classes.sorted : classes.default} />
-                </IconButton>
-              </Tooltip>
+        {!aboutMode && (
+          <React.Fragment>
+            <Tooltip
+              enterDelay={150}
+              leaveDelay={150}
+              placement="bottom"
+              title={sortByDeadline ? "Sort By Priority" : "Sort By Deadline"}
+            >
+              <IconButton onClick={toggleSortByDeadline}>
+                <SortIcon
+                  className={sortByDeadline ? classes.sorted : classes.default}
+                />
+              </IconButton>
+            </Tooltip>
 
-              <span style={{ width: '15px' }} />
-              <AdminButton {...props} label="add" src="assets/icons/plus.png" />
+            <span style={{ width: "15px" }} />
+            <AdminButton {...props} label="add" src="assets/icons/plus.png" />
 
-              <span style={{ width: '15px' }} />
-              <AdminButton {...props} label="filter" src="assets/icons/filter.png" />
-            </React.Fragment>
-        }
+            <span style={{ width: "15px" }} />
+            <AdminButton
+              {...props}
+              label="filter"
+              src="assets/icons/filter.png"
+            />
+          </React.Fragment>
+        )}
 
-        {
-          aboutMode &&
+        {aboutMode && (
+          <Tooltip
+            enterDelay={150}
+            leaveDelay={150}
+            placement="bottom"
+            title="APP"
+          >
             <IconButton onClick={() => setAboutMode(false)}>
-              <ExitIcon style={{ width: '25px', height: '25px' }} />
+              <ExitIcon style={{ width: "25px", height: "25px" }} />
             </IconButton>
-        }
+          </Tooltip>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 TabBar.propTypes = {
   classes: PropTypes.shape({}).isRequired,
   aboutMode: PropTypes.bool.isRequired,
   sortByDeadline: PropTypes.bool.isRequired,
   toggleSortByDeadline: PropTypes.func.isRequired,
-  setAboutMode: PropTypes.func.isRequired,
-}
-
+  setAboutMode: PropTypes.func.isRequired
+};
 
 const AdminButton = ({ expandMode, changeExpandMode, label, src }) => (
-  <div
-    role="button"
-    tabIndex="0"
-    onClick={() => expandMode === label ? changeExpandMode('default') : changeExpandMode(label)}
-    className={scssClasses.imageContainer}
+  <Tooltip
+    enterDelay={300}
+    leaveDelay={300}
+    placement="bottom"
+    title={R.toUpper(label)}
   >
-    <img className={scssClasses.image} alt={label} src={src} />
-  </div>
-)
+    <div
+      role="button"
+      tabIndex="0"
+      onClick={() =>
+        expandMode === label
+          ? changeExpandMode("default")
+          : changeExpandMode(label)
+      }
+      className={scssClasses.imageContainer}
+    >
+      <img className={scssClasses.image} alt={label} src={src} />
+    </div>
+  </Tooltip>
+);
 
 AdminButton.propTypes = {
   expandMode: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   src: PropTypes.string.isRequired,
-  changeExpandMode: PropTypes.func.isRequired,
-}
+  changeExpandMode: PropTypes.func.isRequired
+};
--- frontend/src/components/components/List/main/List.helper.component.jsx
+++ frontend/src/components/components/List/main/List.helper.component.jsx
@@ -1,32 +1,32 @@
 // modules
-import React from 'react'
-import PropTypes from 'prop-types'
-import Tooltip from 'material-ui/Tooltip'
-import Typography from 'material-ui/Typography'
-import { LinearProgress } from 'material-ui/Progress'
+import React from "react";
+import PropTypes from "prop-types";
+import Tooltip from "material-ui/Tooltip";
+import Typography from "material-ui/Typography";
+import { LinearProgress } from "material-ui/Progress";
 // components
-import ActionButtons from '../components/ActionButtons/main/ActionButtons.presentational'
-import SubInfo from '../components/SubInfo/SubInfo.presentational'
-import TextField from '../../../../helper/components/TextField/TextField.presentational'
-import Button from '../../../../helper/components/Button/Button.presentational'
+import ActionButtons from "../components/ActionButtons/main/ActionButtons.presentational";
+import SubInfo from "../components/SubInfo/SubInfo.presentational";
+import TextField from "../../../../helper/components/TextField/TextField.presentational";
+import Button from "../../../../helper/components/Button/Button.presentational";
 // helpers
 import {
   formatTags,
   formatTitle,
   getProgressBarPercent,
-  checkToShow,
-} from './List.helper'
+  checkToShow
+} from "./List.helper";
 import {
   getRemained,
-  getPassedTime,
-} from '../../../../helper/functions/time.helper'
+  getPassedTime
+} from "../../../../helper/functions/time.helper";
 // styles
-import scssClasses from './List.scss'
+import scssClasses from "./List.scss";
 
-export const TitleAndLevelButtons = props => {
+export const TitleAndLevelButtons = (props) => {
   const {
-    task: { title, priority },
-  } = props
+    task: { title, priority }
+  } = props;
 
   return (
     <div className={scssClasses.text}>
@@ -36,15 +36,15 @@ export const TitleAndLevelButtons = props => {
           alt="priority"
           className={scssClasses.priority}
         />
-        <Typography variant="subheading" style={{ marginLeft: '10px' }}>
+        <Typography variant="subheading" style={{ marginLeft: "10px" }}>
           {formatTitle(title) === title ? (
             <span>{formatTitle(title)}</span>
           ) : (
             <Tooltip
               title={title}
               placement="bottom"
-              enterDelay={300}
-              leaveDelay={300}
+              enterDelay={150}
+              leaveDelay={150}
             >
               <span>{formatTitle(title)}</span>
             </Tooltip>
@@ -53,43 +53,43 @@ export const TitleAndLevelButtons = props => {
       </div>
       <ActionButtons {...props} />
     </div>
-  )
-}
+  );
+};
 
 TitleAndLevelButtons.propTypes = {
   task: PropTypes.shape({}).isRequired,
   expandingId: PropTypes.string.isRequired,
-  onExpandClick: PropTypes.func.isRequired,
-}
+  onExpandClick: PropTypes.func.isRequired
+};
 
 export const BriefInfo = ({
   task: { _id, assignee, tags, deadline, sentTime, todos },
-  expandingId,
+  expandingId
 }) =>
   _id !== expandingId && (
     <div className={scssClasses.text}>
       <Typography variant="body2">
-        {checkToShow('assignee') && <span>{assignee}&nbsp;|&nbsp;</span>}
-        {checkToShow('deadline') && <span>{getRemained(deadline)}</span>}
-        {checkToShow('sentTime') && <span>{getPassedTime(sentTime)} ago</span>}
-        <span>&nbsp;|&nbsp;{formatTags(tags) || 'No tags!'}</span>
-        {checkToShow('percent') && (
+        {checkToShow("assignee") && <span>{assignee}&nbsp;|&nbsp;</span>}
+        {checkToShow("deadline") && <span>{getRemained(deadline)}</span>}
+        {checkToShow("sentTime") && <span>{getPassedTime(sentTime)} ago</span>}
+        <span>&nbsp;|&nbsp;{formatTags(tags) || "No tags!"}</span>
+        {checkToShow("percent") && (
           <span>&nbsp;|&nbsp;{`${getProgressBarPercent(todos)}%`}</span>
         )}
       </Typography>
     </div>
-  )
+  );
 
 BriefInfo.propTypes = {
   task: PropTypes.shape({}).isRequired,
-  expandingId: PropTypes.string.isRequired,
-}
+  expandingId: PropTypes.string.isRequired
+};
 
 export const ProgressPanel = ({ todos }) => (
   <div className={scssClasses.progressPannel}>
     <Typography variant="button">
-      {' '}
-      {`${getProgressBarPercent(todos)}%`}{' '}
+      {" "}
+      {`${getProgressBarPercent(todos)}%`}{" "}
     </Typography>
     <LinearProgress
       variant="determinate"
@@ -97,56 +97,56 @@ export const ProgressPanel = ({ todos }) => (
       className={scssClasses.progress}
     />
   </div>
-)
+);
 
 ProgressPanel.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.object).isRequired,
-}
+  todos: PropTypes.arrayOf(PropTypes.object).isRequired
+};
 
 export const FurtherInfo = ({
-  task: { _id, tags, deadline, level, sentTime, assignee, todos },
+  task: { _id, tags, deadline, level, sentTime, assignee, todos }
 }) => (
   <React.Fragment>
     <SubInfo label="TAGS" tags={tags} />
     <SubInfo label="DEADLINE" deadline={deadline} />
-    {(level === 'EVALUATE' || level === 'DONE') && (
+    {(level === "EVALUATE" || level === "DONE") && (
       <SubInfo label="SENT TIME" sentTime={sentTime} />
     )}
     <SubInfo label="ASSIGNEE" assignee={assignee} />
     <SubInfo label="SUBWORKS" todos={todos} _id={_id} level={level} />
   </React.Fragment>
-)
+);
 
 FurtherInfo.propTypes = {
-  task: PropTypes.shape({}).isRequired,
-}
+  task: PropTypes.shape({}).isRequired
+};
 
 export const AddTodo = ({
   task: { level, todoText },
   onTodoTextChange,
-  handleAddTodo,
+  handleAddTodo
 }) =>
-  level === 'ICE BOX' || level === 'IN PROGRESS' ? (
+  level === "ICE BOX" || level === "IN PROGRESS" ? (
     <div className={scssClasses.textField}>
       <TextField
         withButton
         label="New Subtask"
         fullWidth={false}
         value={todoText}
-        onKeyPress={ev => {
-          if (ev.key === 'Enter') {
-            handleAddTodo()
-            ev.preventDefault()
+        onKeyPress={(ev) => {
+          if (ev.key === "Enter") {
+            handleAddTodo();
+            ev.preventDefault();
           }
         }}
-        onChange={e => onTodoTextChange(e.target.value)}
+        onChange={(e) => onTodoTextChange(e.target.value)}
       />
       <Button label="ADD" onClick={handleAddTodo} componentName="Add" />
     </div>
-  ) : null
+  ) : null;
 
 AddTodo.propTypes = {
   task: PropTypes.shape({}).isRequired,
   handleAddTodo: PropTypes.func.isRequired,
-  onTodoTextChange: PropTypes.func.isRequired,
-}
+  onTodoTextChange: PropTypes.func.isRequired
+};
