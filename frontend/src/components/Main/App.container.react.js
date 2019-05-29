11694349e483bb28119cdf71a6d28a4e3254191d
javadvhd <vahedi.r46@gmail.com>
2019-05-29 11:45:59 +0430

    optional-input : discussions resolved

--- frontend/src/components/Main/App.container.react.js
+++ frontend/src/components/Main/App.container.react.js
@@ -1,38 +1,38 @@
 // modules
-import { connect } from "react-redux";
+import { connect } from 'react-redux'
 // components
-import App from "./App.presentational.react";
+import App from './App.presentational.react'
 // views
-import { tabIndexView, numbersObjectView } from "./App.reducer";
-import { expandModeView } from "../components/AppBar/AppBar.reducer";
+import { tabIndexView, numbersObjectView } from './App.reducer'
+import { expandModeView } from '../components/AppBar/AppBar.reducer'
 // actions
 import {
   dispatchChangeTab,
   dispatchSetApi,
-  dispatchFetchInitialData
-} from "./App.action";
-import { dispatchLoadMore } from "../components/List/main/List.action";
+  dispatchFetchInitialData,
+} from './App.action'
+import { dispatchLoadMore } from '../components/List/main/List.action'
 // selectors
-import { getNumberOfTasksInEachLevel, getSortedTasks } from "./App.selector";
-import { tasksView } from "./App.reducer";
+import { getNumberOfTasksInEachLevel, getSortedTasks } from './App.selector'
+// view
+import { tasksView } from './App.reducer'
 
 const mapStateToProps = state => ({
-  // tasks: getSortedTasks(state),
   tasks: tasksView(),
   tabIndex: tabIndexView(),
   expandMode: expandModeView(),
   numbers: getNumberOfTasksInEachLevel(state),
-  numbersObject: numbersObjectView()
-});
+  numbersObject: numbersObjectView(),
+})
 
 const mapDispatchToProps = () => ({
   changeTab: dispatchChangeTab,
   setAPI: dispatchSetApi,
   fetchInitialData: dispatchFetchInitialData,
-  onLoadMore: (skipLength, tabIndex) => dispatchLoadMore(skipLength, tabIndex)
-});
+  onLoadMore: (skipLength, tabIndex) => dispatchLoadMore(skipLength, tabIndex),
+})
 
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
-)(App);
+  mapDispatchToProps,
+)(App)
