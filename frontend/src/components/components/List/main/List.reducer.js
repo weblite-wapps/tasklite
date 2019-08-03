08c1e04faf4d4accbe48c5f5d47aed06b9fb5047
javadvhd <vahedi.r46@gmail.com>
2019-08-03 17:31:20 +0430

    imp(drag-close-expansion): after starting drag expanded tasks will be closed

--- frontend/src/components/components/List/main/List.reducer.js
+++ frontend/src/components/components/List/main/List.reducer.js
@@ -3,7 +3,11 @@ import * as R from 'ramda'
 // local modules
 import { getState } from '../../../../setup/redux'
 // actions
-import { CHANGE_POPOVER_ID, CHANGE_EXPANDING_ID } from './List.action'
+import {
+  CHANGE_POPOVER_ID,
+  CHANGE_EXPANDING_ID,
+  CLOSE_ALL_EXPANDINGS,
+} from './List.action'
 
 // state
 const initialState = {
@@ -13,6 +17,7 @@ const initialState = {
 
 // lens
 const popoverIdLens = R.lensProp('popoverId')
+const expandingIdLens = R.lensProp('expandingId')
 // views
 export const popoverIdView = () => R.path(['List', 'popoverId'])(getState())
 export const expandingIdView = () => R.path(['List', 'expandingId'])(getState())
@@ -25,8 +30,8 @@ const reducers = {
     ...state,
     expandingId: state.expandingId === _id ? '' : _id,
   }),
+  [CLOSE_ALL_EXPANDINGS]: (state, data) => R.set(expandingIdLens, '', state),
 }
-
 
 export default (state = initialState, { type, payload }) =>
   reducers[type] ? reducers[type](state, payload) : state
